88049e52769195e77895a075451517311cf11466
Hendrik Cech <hendrik.cech@gmail.com>
2013-08-24 23:42:19 +0200

    .query API change: profile support - added options object: accepts profiles and maxRefs properties - removed .maxRefs function; added to options object - updated README and tests

--- example/query.js
+++ example/query.js
@@ -1,17 +1,13 @@
 var request = require('../request')
-var meta = require('../test/config.json').meta
-var auth = require('../test/config.json').auth
+var config = require('../test/config.json')
 
-var client = request.createClient(meta)
-var posts = client.query(cb)
-	.limit(1)
-	//.since(1369577987802)
-	//.count()
+var client = request.createClient(config.meta, config.auth)
+var posts = client.query({ profiles: 'all', maxRefs: 5 }, cb)
+
 	
 function cb(err, res, body) {
 	if(err) return console.error(err)
 	console.log(res.statusCode)
-	console.log(body.posts[0])
+	console.log(require('util').inspect(body, { depth: 2, colors: true }))
 }
-
 //posts.pipe(process.stdout)
