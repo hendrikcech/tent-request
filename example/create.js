35ac63a3d4f4ae7cc87756df846080d687620333
Hendrik Cech <hendrik.cech@gmail.com>
2013-08-24 23:50:00 +0200

    updated examples

--- example/create.js
+++ example/create.js
@@ -1,16 +1,14 @@
 var request = require('../request')
-var meta = require('../test/config.json').meta
-var auth = require('../test/config.json').auth
+var config = require('../test/config.json')
 
-var client = request.createClient(meta, auth)
+var client = request.createClient(config.meta, config.auth)
 
 var post = client.create('https://tent.io/types/status/v0#', cb)
-	.content('text', 'Have a nice day.')
-
-//post.pipe(process.stdout)
+	.content('text', 'Status Post')
 
 function cb(err, res, body) {
 	if(err) return console.error(err)
 	console.log(res.statusCode)
 	console.log(body)
 }
+//post.pipe(process.stdout)
--- example/update.js
+++ example/update.js
@@ -2,15 +2,15 @@ var request = require('../request')
 var config = require('../test/config.json')
 
 var client = request.createClient(config.meta, config.auth)
-var req = client.update('JumBJkaEJO6cXA90g8mumg')
+var req = client.update('AB8mjnxlIeJ8n2tP5ztp-w', cb)
+	.parents('sha512t256-0c005935fa828d858d8225f4b85bb553f7bae62')
 	.type('https://tent.io/types/status/v0#')
-	.versionMessage('Update, update!')
+	//.versionMessage('Update, update!')
 	.content({ text: 'New Text!' })
-	.licenses('http://mylicen.se')
 
 function cb(err, res, body) {
 	if(err) return console.error(err)
-	console.log(body.post.content)
-	console.log(body.post.version)
+	console.log(res.statusCode)
+	console.log(require('util').inspect(body, { depth: 10, colors: true }))
 }
 //req.pipe(process.stdout)
