4647e9e9fcec7e34b522845093489c8fc6f743ec
Hendrik Cech <hendrik.cech@gmail.com>
2013-08-24 23:50:00 +0200

    updated examples

--- example/delete.js
+++ example/delete.js
@@ -2,7 +2,7 @@ var request = require('../request')
 var config = require('../test/config.json')
 
 var client = request.createClient(config.meta, config.auth)
-var req = client.delete('9w3naYJZ1m2WX7dvGz2lDA', cb)
+var req = client.delete('j3zIT8syGRRA8eO7p9Dm1w', cb)
 	//.version('732deac42a276d9edf5')
 	//.createDeletePost(false)
 
--- example/get.js
+++ example/get.js
@@ -2,7 +2,7 @@ var request = require('../request')
 var config = require('../test/config.json')
 
 var client = request.createClient(config.meta, config.auth)
-var post = client.get.mentions.count('AB8mjnxlIeJ8n2tP5ztp-w', cb)
+var post = client.get('AB8mjnxlIeJ8n2tP5ztp-w', cb)
 
 function cb(err, res, body) {
 	if(err) return console.error(err)
